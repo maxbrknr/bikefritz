4e97a4dfa57b8fdb0c01ab7d68934d125a693249
Maximilian Birkner <maxxi.birkner@gmail.com>
2019-05-30 15:12:02 +0200

    update offers

--- assets/js/offers.js
+++ assets/js/offers.js
@@ -18,16 +18,6 @@ offers.push(
 offers.push(
     {
         'section': "E-Tour Bikes Modelljahr 2018",
-        'title': "E-Triton PT5 Forma 52cm mit leichtem Lackschaden",
-        'info': "PT5 bedeutet: der Akku ist gut geschützt und elegant im Rahmen integriert.",
-        'price': "3499",
-        'sale': "2900"
-    }
-);
-
-offers.push(
-    {
-        'section': "E-Tour Bikes Modelljahr 2018",
         'title': "E-Lavena PT5 Lady 50cm",
         'info': "PT5 bedeutet: der Akku ist gut geschützt und elegant im Rahmen integriert.",
         'price': "2999",
@@ -37,11 +27,11 @@ offers.push(
 
 offers.push(
     {
-        'section': "E-Tour Bikes Modelljahr 2018",
-        'title': "E-Lavena PT5 Forma 46cm",
-        'info': "PT5 bedeutet: der Akku ist gut geschützt und elegant im Rahmen integriert.",
-        'price': "2999",
-        'sale': "2499"
+        'section': "E-Mountainbikes Modelljahr 2018",
+        'title': "E-Juke+ 20",
+        'info': "",
+        'price': "3199",
+        'sale': "2999"
     }
 );
 
@@ -52,16 +42,6 @@ offers.push(
         'info': "",
         'price': "2299",
         'sale': "1799"
-    }
-);
-
-offers.push(
-    {
-        'section': "E-Mountainbikes Modelljahr 2018",
-        'title': "E-Stoke-Lady",
-        'info': "",
-        'price': "2499",
-        'sale': "1999"
     }
 );
 
@@ -87,21 +67,131 @@ offers.push(
 
 offers.push(
     {
-        'section': "Mountainbikes Modelljahr 2018",
-        'title': "Colorado",
+        'section': "MTB",
+        'title': "Superleichte Kinder-MTBs in allen Farben in 20\" und mit 24\"-Laufrädern",
         'info': "",
-        'price': "1999",
-        'sale': "1599"
+        'price': "399",
+        'sale': "349"
     }
 );
 
 offers.push(
     {
-        'section': "Mountainbikes Modelljahr 2018",
-        'title': "Sentiero",
+        'section': "MTB",
+        'title': "Nema 16\"",
+        'info': "",
+        'price': "699",
+        'sale': "649"
+    }
+);
+
+offers.push(
+    {
+        'section': "MTB",
+        'title': "Jura Carbon Fully Modelljahr 2018",
+        'info': "",
+        'price': "2999",
+        'sale': "2199"
+    }
+);
+
+offers.push(
+    {
+        'section': "MTB",
+        'title': "Jura Carbon ES Fully Modelljahr 2018",
+        'info': "",
+        'price': "4299",
+        'sale': "2899"
+    }
+);
+
+offers.push(
+    {
+        'section': "MTB",
+        'title': "Sentiero Modelljahr 2018",
         'info': "",
         'price': "1499",
         'sale': "1199"
+    }
+);
+
+offers.push(
+    {
+        'section': "Crossbikes",
+        'title': "X3",
+        'info': "",
+        'price': "499",
+        'sale': "449"
+    }
+);
+
+offers.push(
+    {
+        'section': "Crossbikes",
+        'title': "X5",
+        'info': "",
+        'price': "799",
+        'sale': "739"
+    }
+);
+
+offers.push(
+    {
+        'section': "Crossbikes",
+        'title': "X6 Lady disc 46cm, weiß Modelljahr 2018",
+        'info': "",
+        'price': "999",
+        'sale': "799"
+    }
+);
+
+offers.push(
+    {
+        'section': "Rennräder",
+        'title': "Ventoux disc, kompl. Ultegra 52, 54 und 58cm",
+        'info': "",
+        'price': "3099",
+        'sale': "2299"
+    }
+);
+
+offers.push(
+    {
+        'section': "Rennräder",
+        'title': "Aspin 56cm",
+        'info': "",
+        'price': "1599",
+        'sale': "1399"
+    }
+);
+
+offers.push(
+    {
+        'section': "Vorführräder",
+        'title': "FAT-bike Mobster 18\"",
+        'info': "",
+        'price': "1499",
+        'sale': "890"
+    }
+);
+
+offers.push(
+    {
+        'section': "Vorführräder",
+        'title': "Cyclocross Namur 52cm",
+        'info': "",
+        'price': "1199",
+        'sale': "890"
+    }
+);
+
+offers.push(
+    {
+        'section': "Vorführräder",
+        'title': "Rennrad Ventoux disc 56cm",
+        'info': "",
+        'price': "3200",
+        'sale': "2199"
     }
 );
 
